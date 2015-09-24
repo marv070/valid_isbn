dae51f02022286981b7b2514c0e03f127845d215
Marvin Laucher <marv070@gmail.com>
2015-09-24 12:03:55 -0400

    testing for x as check num

--- valid_isbn.rb
+++ valid_isbn.rb
@@ -1,11 +1,15 @@
 def isbn_checker(book_number)
-  
-  remove_spaces_from_isbn(book_number)
-  remove_dashes_from_isbn(book_number)
-  length_verifier(book_number)
-  #multiply_each_digit_by_its_position_if_10_digits(book_number)
+   no_spaces_book_number= remove_spaces_from_isbn(book_number)
+   no_dashes_or_spaces_book_number= remove_dashes_from_isbn(no_spaces_book_number) 
+  if length_verifier(no_dashes_or_spaces_book_number)==true
+    isbn_array=book_number_array(no_dashes_or_spaces_book_number)
+  check_digit_valid_10(isbn_array)
+  else
+    false
+  end
 end
-
+ 
+ 
 def length_verifier(book_number)
   if book_number.length==10
     true
@@ -14,21 +18,44 @@ def length_verifier(book_number)
   else
     false
   end
-  end
+end
+  
+ def book_number_array(book_number)
+  book_number_array=book_number.split(//,)
+end
 
-def remove_dashes_from_isbn(book_number)
-  if book_number.include?"-"
-	book_number.delete!"-"
-	else
-	true
+def remove_dashes_from_isbn(book_number_array)
+  if book_number_array.include?"-"
+    no_dashes_isbn = book_number_array.delete!"-"
+  else
+    book_number_array
   end
-  end
+book_number_array
+end
 
-def remove_spaces_from_isbn(book_number)
-  if book_number.include?" "
-     book_number.delete!" "
-	 else
-	 true
+def remove_spaces_from_isbn(book_number_array)
+  if book_number_array.include?" "
+      book_number_array.delete!" "
   end
+    book_number_array
+end
+  
+
+def check_digit_contains_X(isbn)
+isbn_number = isbn.split ""
+
+  if isbn_number[9] == "x" or isbn_number[9] == "X"
+  isbn_number[9] = 10
   end
   
+isbn_number[9] == 10
+
+end
+
+  
+
+
+
+
+
+
--- valid_isbn_test.rb
+++ valid_isbn_test.rb
@@ -3,27 +3,35 @@ require"minitest/autorun"
  
 class Isbn_verify<Minitest::Test  
 
-def test_for_ten_digits
-assert_equal(true,isbn_checker("1234567892"))
-assert_equal(false,isbn_checker("123456789234"))
-assert_equal(true,isbn_checker("123 456 789 1"))
-end
+  def test_for_ten_digits
+    assert_equal(true,length_verifier("1234567892"))
+    assert_equal(false,length_verifier("123456789234"))
+    assert_equal(true,length_verifier("1234567891"))
+  end
 
-def test_for_thirteen_digits
-assert_equal(true,isbn_checker("1234567891234"))
-assert_equal(false,isbn_checker("152347689"))
-end
+  def test_for_thirteen_digits
+    assert_equal(true,length_verifier("1234567891234"))
+    assert_equal(false,length_verifier("152347689"))
+  end
 
-def test_for_removing_dashes_from_isbn
-assert_equal(true,isbn_checker("123-456-789-1"))
-end
+  def test_for_removing_dashes_from_isbn
+    assert_equal("1234567891",remove_dashes_from_isbn("123-456-789-1"))
+  end
 
-def test_for_removing_spaces_from_isbn
-assert_equal(true,isbn_checker("987 654 321 9"))
-end
+  def test_for_removing_spaces_from_isbn
+    assert_equal("9876543219",remove_spaces_from_isbn("987 654 321 9"))
+  end
 
-# def test_for_multiplying_each_number_by_its_position_if_10_digits
-# assert_equal(9,isbn_checker("123-123-123-1"))
-# end
+  
+  def test_string_convert_to_array
+    assert_equal(["1","2","3","3","2","1","4","5","6","7"],book_number_array("1233214567"))
+  end
+
+  def test_for_x
+  assert_equal(true, check_digit_contains_X("080442957X"))
+  end
+
+
+
 
 end
