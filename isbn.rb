e32d0f60b628d9df157e817d2d3e920c5c34c0d8
Marvin Laucher <marv070@gmail.com>
2015-10-07 21:31:28 -0400

    new updates of code from wed class. work in progress

--- isbn.rb
+++ isbn.rb
@@ -1,90 +1,35 @@
-require"csv"
 
-def read_file_and_output
-    file_old = File.open("input_isbn_file.csv","r")
-    file_name = "isbn_output_test.csv"
-    file_new = File.open(file_name, "w")
-    file_old.each do |line|remove_dashes_and_spaces_from_isbn(line)|
-        if @output == true
-          file_new.puts line.chomp + ",valid"
-        else @output == false
-          file_new.puts line.chomp + ",invalid"
-                    end
-        end
+
+# def read_file_and_output
+#     file_old = File.open("input_isbn_file.csv","r")
+#     file_name = "isbn_output_test.csv"
+#     file_new = File.open(file_name, "w")
+#     file_old.each do |line|remove_dashes_and_spaces_from_isbn(line)
+#         if @output == true
+#           file_new.puts line.chomp + ",valid"
+#         else @output == false
+#           file_new.puts line.chomp + ",invalid"
+#                     end
+#         end
     
-    file_new.close
+#     file_new.close
 
-end
+#end
 
-def remove_dashes_and_spaces_from_isbn(isbn_number)
-    if isbn_number.include? "-"
-      isbn_number.delete! '-'
-    elsif isbn_number.include?" "
-     isbn_number.delete! ' ' 
-    
-    else 
-      isbn_number
-   end
-remove_extra_characters(isbn_number)    
-end
+def valid_isbn?(any_string)
+  no_dashes_or_spaces = remove_dashes_and_spaces_from_isbn(any_string)
+  if no_dashes_or_spaces.length == 10
+  valid_isbn_10?(no_dashes_or_spaces) 
 
-def remove_extra_characters(isbn_number)
-    isbn_number_array = isbn_number.split ("")
-    isbn_number_array.delete_at(0)
-    isbn_number_array.delete_at(-1)
-    isbn_number_array.delete_at(-1)
-    # isbn_number_array=isbn_number_array.join("")
-     verify_length(isbn_number_array)
-end
-def verify_length(isbn_number)
-    @isbn_arr = isbn_number 
-    
-    if
-     @isbn_arr.length == 10
-      check_digit_contains_X(@isbn_arr)              
-      if only_numeric_characters(@new_isbn_number) == true
-        @output = false
-      else
-        check_digit_10_is_valid(@new_isbn_number)
-      end
-    elsif 
-      @isbn_arr.length == 13 #changed to @isbn_arr
-      if only_numeric_characters(@isbn_arr) == true 
-        @output = false
-       else
-        check_digit_13_is_valid(@isbn_arr)#changed 
-      end
-    else
-      @output = false
-    end
+
+  else
+     false
+  end
  
 end
-    
-def check_digit_contains_X(isbn_arr)#changed isbn_number_array
-#isbn_number_array=isbn_number_array.split ""
 
-    if isbn_arr[9] == "x" or isbn_arr[9] == "X"
-      isbn_arr[9] = 10 
-      
-    end
- @new_isbn_number = isbn_arr              
-
-end
-
-
-def only_numeric_characters(isbn_number)#(isbn_number)
-    book_num = isbn_number.join("")
-    if book_num =~ /\D/
-       return true 
-    else 
-       return false
-    end
-end
-
-def check_digit_10_is_valid(isbn_arr)#(isbn_number_array)
-#isbn_number_array = isbn_number_array.split("")
-
-array =[]
+def valid_isbn_10?(no_dashes_or_spaces)
+    array =[]
 
     isbn_arr.each do |value|
     array << value.to_i 
@@ -101,46 +46,150 @@ sum = 0
 check_digit = sum%11
 
     if check_digit == array[9]
-    @output = true
+     true
     else
-    @output = false
+     false
     end
     
 end
 
-def check_digit_13_is_valid(isbn_arr)
-#isbn_number_array = isbn_number_array.split ""
-array =[]
- 
-  isbn_arr.each do |value|
-  array << value.to_i
-  end
- 
-sum = 0
-check_digit = 0
- 
-  array.each_with_index do |value,index|
-    break if index == 12
-        if index  %2 == 0
-          sum += value * 1
-         else
-          sum += value * 3
-        end    
-end        
-      sum = sum %10
-  check_digit=(10-sum)
+
+
+def remove_dashes_and_spaces_from_isbn(isbn_number)
+    disallowed_characters = [ " ", "-"]
     
-    if check_digit == 10
-       check_digit = 0
+    disallowed_characters.each do | c|
+      isbn_number.delete! c if isbn_number.include? c
     end
+    isbn_number 
+end
+
+
+# def remove_extra_characters(isbn_number)
+#     isbn_number_array = isbn_number.split ("")
+#     isbn_number_array.delete_at(0)
+#     isbn_number_array.delete_at(-1)
+#     isbn_number_array.delete_at(-1)
+#      #isbn_number_array=isbn_number_array.join("")
+#      verify_length(isbn_number_array)
+# end
+
+def verify_length(isbn_number)
+  isbn_number.length == 10 || isbn_number.length == 13
+end
+
+
+
+
+
+
+
+
+# def verify_length(isbn_number)
+#     @isbn_arr = isbn_number 
     
-    if array[12] == check_digit
-      @output = true
-    else
-      @output = false
-    end
+#     if
+#      @isbn_arr.length == 10
+#       check_digit_contains_X(@isbn_arr)              
+#       if only_numeric_characters(@new_isbn_number) == true
+#         @output = false
+#       else
+#         check_digit_10_is_valid(@new_isbn_number)
+#       end
+#     elsif 
+#       @isbn_arr.length == 13 #changed to @isbn_arr
+#       if only_numeric_characters(@isbn_arr) == true 
+#         @output = false
+#        else
+#         check_digit_13_is_valid(@isbn_arr)#changed 
+#       end
+#     else
+#       @output = false
+#     end
+ 
+# end
+    
+# def check_digit_contains_X(isbn_arr)#changed isbn_number_array
+# #isbn_number_array=isbn_number_array.split ""
+
+#     if isbn_arr[9] == "x" or isbn_arr[9] == "X"
+#       isbn_arr[9] = 10 
+      
+#     end
+#  @new_isbn_number = isbn_arr              
+
+# end
+
+
+# def only_numeric_characters(isbn_number)#(isbn_number)
+#     book_num = isbn_number.join("")
+#     if book_num =~ /\D/
+#        return true 
+#     else 
+#        return false
+#     end
+# end
+
+# def check_digit_10_is_valid(isbn_arr)#(isbn_number_array)
+# #isbn_number_array = isbn_number_array.split("")
+
+# array =[]
+
+#     isbn_arr.each do |value|
+#     array << value.to_i 
+#     end
+    
+
+# sum = 0
+
+#     array.each.with_index do |value, index|
+#     break if index == 9
+#     sum += value * (index + 1)
+#     end
+
+# check_digit = sum%11
+
+#     if check_digit == array[9]
+#     @output = true
+#     else
+#     @output = false
+#     end
+    
+# end
+
+# def check_digit_13_is_valid(isbn_arr)
+# #isbn_number_array = isbn_number_array.split ""
+# array =[]
+ 
+#   isbn_arr.each do |value|
+#   array << value.to_i
+#   end
+ 
+# sum = 0
+# check_digit = 0
+ 
+#   array.each_with_index do |value,index|
+#     break if index == 12
+#         if index  %2 == 0
+#           sum += value * 1
+#          else
+#           sum += value * 3
+#         end    
+# end        
+#       sum = sum %10
+#   check_digit=(10-sum)
+    
+#     if check_digit == 10
+#        check_digit = 0
+#     end
+    
+#     if array[12] == check_digit
+#       @output = true
+#     else
+#       @output = false
+#     end
         
-end
+# end
 
 
-read_file_and_output
+# read_file_and_output
--- valid_isbn_test.rb
+++ valid_isbn_test.rb
@@ -3,48 +3,71 @@ require"minitest/autorun"
  
 class Isbn_verify<Minitest::Test  
 
-def test_for_valid_amount_of_digits
-  #assert_equal(true,verify_length("1234567890"))
-  assert_equal(false,verify_length(["1","0","2","9","3","8","4","7","5","7"]))
-  #assert_equal(true,verify_length("0-321-14653-0"))
+# def test_for_valid_amount_of_digits
+#   assert_equal(true,verify_length("1234567890"))
+#   assert_equal(false,verify_length( "134689763"))
+#   assert_equal(true,verify_length("0321146530987"))
+# end
+
+
+# def test_removes_dashes_from_isbn
+#   assert_equal("0321146530",remove_dashes_and_spaces_from_isbn("0-321-14653-0"))
+#   end
+
+
+# def test_to_remove_spaces
+#     assert_equal("1234567890", remove_dashes_and_spaces_from_isbn("123 4567 890"))
+#     assert_equal("1237362781", remove_dashes_and_spaces_from_isbn("123 7362 781"))
+# end 
+
+# def test_with_spaces_and_hyphens
+#     assert_equal("1234567890", remove_dashes_and_spaces_from_isbn("1 2-3-4-5678-9 0"))
+# end
+
+def test_input_string_is_a_valid_isbn1
+    
+    assert_equal(false,valid_isbn?("0471958698"))
 end
 
-
-def test_removes_dashes_from_isbn
-  assert_equal("0321146530",remove_dashes_and_spaces_from_isbn("0-321-14653-0"))
-  end
-
-
-def test_to_remove_spaces
-    assert_equal("1234567890", remove_dashes_and_spaces_from_isbn("123 4567 890"))
-    assert_equal("1237362781", remove_dashes_and_spaces_from_isbn("123 7362 781"))
-end 
-
-def test_for_X_in_ISBN
-   assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], check_digit_contains_X([1,2,3,4,5,6,7,8,9,"x"]))
-   assert_equal([1, 2, 3, 4, 5, 5, 5, 7, 8, 1], check_digit_contains_X([1,2,3,4,5,5,5,7,8,1]))
+def test_input_string_is_a_valid_isbn2
+    assert_equal(true,valid_isbn?("0 4 7-1958697"))
 end
 
-def test_for_only_numeric_characters
-  assert_equal(false, only_numeric_characters(["1","2","3","4","5","6","7","8","9","0"]))
-  assert_equal(false, only_numeric_characters(["1","2","3","4","5","6","7","8","9","10"]))
-  assert_equal(false, only_numeric_characters(["1","2"]))
-  assert_equal(true, only_numeric_characters(["a","b","d","1","2","3","d","e","f","1","2","3","0"]))
+def test_input_string_is_a_valid_isbn3
+    assert_equal(true,valid_isbn?("0471958697"))
+    
 end
+
+def test_input_string_is_a_valid_isbn4
+    assert_equal(false,valid_isbn?("1471958697"))
+    
+end
+
+# def test_for_X_in_ISBN
+#    assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], check_digit_contains_X([1,2,3,4,5,6,7,8,9,"x"]))
+#    assert_equal([1, 2, 3, 4, 5, 5, 5, 7, 8, 1], check_digit_contains_X([1,2,3,4,5,5,5,7,8,1]))
+# end
+
+# def test_for_only_numeric_characters
+#   assert_equal(false, only_numeric_characters(["1","2","3","4","5","6","7","8","9","0"]))
+#   assert_equal(false, only_numeric_characters(["1","2","3","4","5","6","7","8","9","10"]))
+#   assert_equal(false, only_numeric_characters(["1","2"]))
+#   assert_equal(true, only_numeric_characters(["a","b","d","1","2","3","d","e","f","1","2","3","0"]))
+# end
   
-def test_for_check_digit_10
-  assert_equal(true, check_digit_10_is_valid(["0","3","0","6","4","0","6","1","5","2"]))
-  assert_equal(false, check_digit_10_is_valid([1,2,4,6,7,8,9,0,9,9]))
-  assert_equal(true, check_digit_10_is_valid([0,3,2,1,1,4,6,5,3,0]))
-end
+# def test_for_check_digit_10
+#   assert_equal(true, check_digit_10_is_valid(["0","3","0","6","4","0","6","1","5","2"]))
+#   assert_equal(false, check_digit_10_is_valid([1,2,4,6,7,8,9,0,9,9]))
+#   assert_equal(true, check_digit_10_is_valid([0,3,2,1,1,4,6,5,3,0]))
+# end
   
 
- def test_for_thirteen_character_math
-    assert_equal(true, check_digit_13_is_valid([9,7,8,0,1,5,6,0,2,7,3,2,8]))
-    assert_equal(false, check_digit_13_is_valid([9,7,8,0,4,7,0,0,0,9,0,2,9]))
-    assert_equal(false, check_digit_13_is_valid([1,3,4,5,6,7,8,9,1,1,1,1,2]))
-    assert_equal(true, check_digit_13_is_valid([9,7,8,0,4,7,0,0,5,9,0,2,9]))
-  end 
+#  def test_for_thirteen_character_math
+#     assert_equal(true, check_digit_13_is_valid([9,7,8,0,1,5,6,0,2,7,3,2,8]))
+#     assert_equal(false, check_digit_13_is_valid([9,7,8,0,4,7,0,0,0,9,0,2,9]))
+#     assert_equal(false, check_digit_13_is_valid([1,3,4,5,6,7,8,9,1,1,1,1,2]))
+#     assert_equal(true, check_digit_13_is_valid([9,7,8,0,4,7,0,0,5,9,0,2,9]))
+#   end 
   
   
   
